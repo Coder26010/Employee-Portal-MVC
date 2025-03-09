c71700fd08fae1cfd7ecf66bedb6cb0e08caeb64
Kund0249 <k.kundan1999@gmail.com>
2025-03-09 10:14:13 +0530

    Add project files.

---
+++ Employee-Portal-MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using Employee_Portal_MVC.Models;
+using System;
+using System.Web.Mvc;
+
+
+namespace Employee_Portal_MVC.Controllers
+{
+    //Controller => Employee
+    public class EmployeeController : Controller
+    {
+        //Action Methods   => Employee/GetMessage
+        //Action Methods must be Public
+        //Action Methods must be non-static
+        //Action Methods can not be overloaded like a c# Method
+        //Action Methods are Http Verbs based that handle http request and response
+
+        //[HttpGet] //This is GET Http verbs that handles only GET request
+        //public string GetMessage()
+        //{
+        //    return "Hello from GetMessage Method";
+        //}
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View("EmployeeCreate");
+        }
+
+        //[HttpPost]
+        //public ViewResult Create(string EmpName,string EmailAddress,string Gender)
+        //{
+        //    return View("EmployeeCreate");
+        //}
+
+        [HttpPost]
+        public ViewResult Create(EmployeeModel model)
+        {
+            return View("EmployeeCreate");
+        }
+
+    }
+}
---
+++ Employee-Portal-MVC/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee_Portal_MVC.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+    }
+}
